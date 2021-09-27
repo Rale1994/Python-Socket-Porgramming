b849039efba877820332f586c958abee7929c3b7
unknown <golrale3@gmail.com>
2021-09-27 22:02:08 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client_socket.send(send_length)
+    client_socket.send(message)
+    print(client_socket.recv(2048).decode(FORMAT))
+
+
+send("Hello World!")
